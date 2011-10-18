42d76ffff455efc2dfc1db3641c87cf33419b20d
Maxim Treskin <zerthurd@gmail.com>
2011-10-18 17:18:40 +0700

    Add pid to DBG

--- include/mt_log.hrl
+++ include/mt_log.hrl
@@ -1,6 +1,6 @@
 -ifndef(MT_LOG_H).
 -define(MT_LOG_H, true).
 
--define(DBG(F, A), io:format("(~w:~b) " ++ F ++ "~n", [?MODULE, ?LINE | A])).
+-define(DBG(F, A), io:format("(~w:~b) ~p " ++ F ++ "~n", [?MODULE, ?LINE, self() | A])).
 
 -endif. % MT_LOG_H

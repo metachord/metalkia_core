552f606032fecc03a73b1d3f9cddc96aa168948f
Maxim Treskin <zerthurd@gmail.com>
2011-11-02 13:45:56 +0700

    Add ERR macro

--- include/mt_log.hrl
+++ include/mt_log.hrl
@@ -2,5 +2,6 @@
 -define(MT_LOG_H, true).
 
 -define(DBG(F, A), io:format("(~w:~b) ~p " ++ F ++ "~n", [?MODULE, ?LINE, self() | A])).
+-define(ERR(F, A), io:format("*** ERROR (~w:~b) ~p " ++ F ++ "~n", [?MODULE, ?LINE, self() | A])).
 
 -endif. % MT_LOG_H

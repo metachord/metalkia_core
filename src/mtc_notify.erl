a5c2a5d9bf4efee223f28b0411b366a71bcd2c0d
Maxim Treskin <zerthurd@gmail.com>
2011-12-13 18:03:54 +0700

    Improve email notificator

--- src/mtc_notify.erl
+++ src/mtc_notify.erl
@@ -12,14 +12,60 @@ send(email, To, {From, Subj, Text}) when
 send(email, #mt_person{email = To}, {From, Subj, Text}) ->
   email(To, From, Subj, Text).
 
-email(To, From, Subj, Text) ->
-  Message = io_lib:format(
-    "to:~ts\n"
-    "from:~ts\n"
-    "subject:~ts\n"
-    "\n"
-    "~ts",
-    [To, From, Subj, Text]),
+email(To, From, Subj, Data) ->
+  ?DBG("Send mail to ~p", [From]),
+
+  Enq =
+  fun(T) ->
+    re:replace(T, "=", "=3D", [global, {return, list}, unicode])
+  end,
+
+  F0 = "To: ~ts\n"
+  "From: ~ts\n"
+  "Subject: ~ts\n"
+  "X-Mailer: Metalkia Email Notify\n"
+  "Date: " ++ httpd_util:rfc1123_date() ++ "\n",
+
+  Message =
+  case Data of
+    {TextData, HtmlData} ->
+      Boundary = "_---------=_" ++ mtc_util:rand_str(30),
+      io_lib:format(
+        F0 ++
+        "MIME-Version: 1.0\n"
+        "Content-Transfer-Encoding: binary\n"
+        "Content-Type: multipart/alternative; boundary=\"" ++ Boundary ++ "\"\n"
+        "\n"
+        "This is a multi-part message in MIME format.\n"
+        "\n"
+        "--" ++ Boundary ++ "\n"
+        "Content-Disposition: inline\n"
+        "Content-Type: text/plain; charset=\"utf-8\"\n"
+        "Content-Transfer-Encoding: quoted-printable\n"
+        "\n"
+        "~ts"
+        "\n"
+        "--" ++ Boundary ++ "\n"
+        "Content-Disposition: inline\n"
+        "Content-Type: text/html; charset=\"utf-8\"\n"
+        "Content-Transfer-Encoding: quoted-printable\n"
+        "\n"
+        "<head><meta http-equiv=\"Content-Type\" content=\"text/html\" /></head>\n"
+        "<body>\n"
+        "~ts\n"
+        "</body>\n"
+        "\n"
+        "--" ++ Boundary ++ "--"
+        "\n",
+        [To, From, Subj, Enq(TextData), Enq(HtmlData)]);
+    TextData ->
+      io_lib:format(
+        F0 ++
+        "\n"
+        "~ts"
+        "\n",
+        [To, From, Subj, TextData])
+  end,
   Port = open_port({spawn, mtc:get_env(sendmail, "/usr/sbin/sendmail -t")}, [use_stdio, exit_status, binary]),
   port_command(Port, unicode:characters_to_binary(Message)),
   port_close(Port).

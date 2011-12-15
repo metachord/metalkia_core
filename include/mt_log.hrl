7e990d2e7f57f4301fb9310b4991142700c5115d
Maxim Treskin <zerthurd@gmail.com>
2011-12-15 15:51:36 +0700

    Use error_logger for debugging

--- include/mt_log.hrl
+++ include/mt_log.hrl
@@ -1,7 +1,8 @@
 -ifndef(MT_LOG_H).
 -define(MT_LOG_H, true).
 
--define(DBG(F, A), io:format("(~w:~b) ~p " ++ F ++ "~n", [?MODULE, ?LINE, self() | A])).
--define(ERR(F, A), io:format("*** ERROR (~w:~b) ~p " ++ F ++ "~n", [?MODULE, ?LINE, self() | A])).
+-define(DBG(F, A), error_logger:info_msg("(~w:~b) ~p " ++ F ++ "~n", [?MODULE, ?LINE, self() | A])).
+-define(WRN(F, A), error_logger:watning_msg("(~w:~b) ~p " ++ F ++ "~n", [?MODULE, ?LINE, self() | A])).
+-define(ERR(F, A), error_logger:error_msg("*** ERROR (~w:~b) ~p " ++ F ++ "~n", [?MODULE, ?LINE, self() | A])).
 
 -endif. % MT_LOG_H

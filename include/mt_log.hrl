185bd813d7bdf4b62f7a78940648ad9d1fc8d3a9
Maxim Treskin <zerthurd@gmail.com>
2012-05-11 09:18:37 +0700

    Use lager logger

--- include/mt_log.hrl
+++ include/mt_log.hrl
@@ -1,8 +1,11 @@
--ifndef(MT_LOG_H).
--define(MT_LOG_H, true).
+-ifndef(_MT_LOG_H).
+-define(_MT_LOG_H, true).
 
--define(DBG(F, A), error_logger:info_msg("(~w:~b) ~p " ++ F ++ "~n", [?MODULE, ?LINE, self() | A])).
--define(WRN(F, A), error_logger:watning_msg("(~w:~b) ~p " ++ F ++ "~n", [?MODULE, ?LINE, self() | A])).
--define(ERR(F, A), error_logger:error_msg("*** ERROR (~w:~b) ~p " ++ F ++ "~n", [?MODULE, ?LINE, self() | A])).
+-compile([{parse_transform, lager_transform}]).
 
--endif. % MT_LOG_H
+-define(DBG(F, A), lager:debug("(~w:~b) ~p " ++ F ++ "~n", [?MODULE, ?LINE, self() | A])).
+-define(INFO(F, A), lager:info("(~w:~b) ~p " ++ F ++ "~n", [?MODULE, ?LINE, self() | A])).
+-define(ERR(F, A), lager:error("(~w:~b) ~p " ++ F ++ "~n", [?MODULE, ?LINE, self() | A])).
+-define(WARN(F, A), lager:warning("(~w:~b) ~p " ++ F ++ "~n", [?MODULE, ?LINE, self() | A])).
+
+-endif. % _MT_LOG_H
